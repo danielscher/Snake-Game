a1ef04c11d25e818e04ffedfcf96a89b6837bbfd
DanielPC <dash00001@stud.uni-saarland.de>
2023-10-17 18:39:43 +0200

    replaced old main function with new.

---
+++ src/main/java/gui/Main.java
@@ -0,0 +1,30 @@
+package gui;
+
+import controller.MainController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        URL url = getClass().getResource("/snakeGame.fxml");
+        FXMLLoader loader = new FXMLLoader(url);
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+        MainController controller = loader.getController();
+        controller.initData(stage);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
